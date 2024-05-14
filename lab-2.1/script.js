6e225dc776767d6353ba5aa0e5e52f55e3f5d0d3
Tyler Hazzard <tkhazzard2003@gmail.com>
2024-05-13 20:06:01 -0500

    js refresh

---
+++ lab-2.1/script.js
@@ -0,0 +1,33 @@
+const firstname = 'tyler';
+const lastname = 'hazzard';
+const fullname = firstname + " " + lastname;
+
+console.log(`${fullname}`);
+
+let population = 673104;
+
+console.log(`${population}`);
+
+population *= 3;
+
+console.log(`${population}`);
+
+const javascriptiscool = true;
+
+const colors = [
+    "red", 
+    "orange", 
+    "yellow", 
+    "green", 
+    "blue", 
+    "indigo", 
+    "violet"
+]
+
+for (let i = 1; i <= 5; i++) {
+    console.log(i)
+}
+
+for (let i = 0; i < 5; i++) {
+    console.log(i)
+}
